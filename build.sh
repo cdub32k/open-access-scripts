e62e9070e030cad1e3b5d1ca02cc22233c982bbc
Chris Wood <chriswood@chriss-mbp.home>
2020-06-23 01:54:45 -0700

    push to prod s3 bucket

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 npm run build --prefix open-access-frontend
 mv open-access-frontend/dist/index.html open-access-backend
 
-aws s3 sync ./open-access-frontend/dist s3://open-access-dev/app --acl "public-read" --exclude "*" --include "*.js" --content-encoding gzip
-aws s3 sync ./open-access-frontend/dist s3://open-access-dev/app --acl "public-read" --exclude "*" --include "*.css" --content-encoding gzip
-aws s3 sync ./open-access-frontend/dist/assets s3://open-access-dev/app/assets --acl "public-read"
+aws s3 sync ./open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.js" --content-encoding gzip
+aws s3 sync ./open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.css" --content-encoding gzip
+aws s3 sync ./open-access-frontend/dist/assets s3://open-access-network/app/assets --acl "public-read"
 
 git --git-dir=./open-access-backend/.git --work-tree=./open-access-backend checkout master 
 git --git-dir=./open-access-backend/.git --work-tree=./open-access-backend add index.html
