a56130652c13e09ea5eeecd1dc16d52a1c49336e
cdub32k <cwood32k@gmail.com>
2020-09-03 22:16:00 -0700

    update file paths of fe, be codebases

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 npm run build --prefix open-access-frontend
 mv open-access-frontend/dist/index.html open-access-backend
 
-aws s3 sync ./open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.js" --content-encoding gzip
-aws s3 sync ./open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.css" --content-encoding gzip
-aws s3 sync ./open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "favicon.ico"
+aws s3 sync ../open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.js" --content-encoding gzip
+aws s3 sync ../open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.css" --content-encoding gzip
+aws s3 sync ../open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "favicon.ico"
 
-aws s3 sync ./open-access-frontend/dist/assets s3://open-access-network/app/assets --acl "public-read"
+aws s3 sync ../open-access-frontend/dist/assets s3://open-access-network/app/assets --acl "public-read"
 
-git --git-dir=./open-access-backend/.git --work-tree=./open-access-backend checkout master 
-git --git-dir=./open-access-backend/.git --work-tree=./open-access-backend add index.html
-git --git-dir=./open-access-backend/.git --work-tree=./open-access-backend commit -m "server build"
-git --git-dir=./open-access-backend/.git --work-tree=./open-access-backend push origin master
+git --git-dir=../open-access-backend/.git --work-tree=../open-access-backend checkout master 
+git --git-dir=../open-access-backend/.git --work-tree=../open-access-backend add index.html
+git --git-dir=../open-access-backend/.git --work-tree=../open-access-backend commit -m "server build"
+git --git-dir=../open-access-backend/.git --work-tree=../open-access-backend push origin master
