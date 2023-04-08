0180d711592e74a95d0a2a8b59604261cf8a2eb2
Chris Wood <cwood32k@gmail.com>
2023-04-07 21:52:14 -0700

    Update file paths to reflect new directory structure

--- build-dev.sh
+++ build-dev.sh
@@ -1,5 +1,5 @@
-npm run build-dev --prefix open-access-frontend
-mv open-access-frontend/dist/index.html open-access-backend
+npm run build-dev --prefix ../open-access-frontend
+mv ../open-access-frontend/dist/index.html ../open-access-backend
 
 aws s3 sync ../open-access-frontend/dist s3://open-access-dev/app --acl "public-read" --exclude "*" --include "*.js" --content-encoding gzip
 aws s3 sync ../open-access-frontend/dist s3://open-access-dev/app --acl "public-read" --exclude "*" --include "*.css" --content-encoding gzip
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-npm run build --prefix open-access-frontend
-mv open-access-frontend/dist/index.html open-access-backend
+npm run build --prefix ../open-access-frontend
+mv ../open-access-frontend/dist/index.html ../open-access-backend
 
 aws s3 sync ../open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.js" --content-encoding gzip
 aws s3 sync ../open-access-frontend/dist s3://open-access-network/app --acl "public-read" --exclude "*" --include "*.css" --content-encoding gzip
